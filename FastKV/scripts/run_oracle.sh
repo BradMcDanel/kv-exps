6b42104cf0d05ebcb3306b15a7059a078510c8c2
Brad McDanel <mcdabw6@gmail.com>
2025-07-15 11:04:01 -0400

    .

---
+++ FastKV/scripts/run_oracle.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+model="meta-llama/Llama-3.1-8B-Instruct"
+dataset_list="qasper"
+device=0
+keep_percentage=0.1
+oracle_rankings_path="analysis_results/oracles"
+path="oracle"
+
+for dataset in $dataset_list
+do
+    CUDA_VISIBLE_DEVICES=$device python -m eval.longbench.main \
+        --model $model \
+        --mode oracle \
+        --save_path $path \
+        --dataset $dataset \
+        --keep_percentage $keep_percentage \
+        --oracle_rankings_path $oracle_rankings_path
+
+    CUDA_VISIBLE_DEVICES=$device python -m eval.longbench.evaluate \
+        --model $model \
+        --eval_path $path
+done
